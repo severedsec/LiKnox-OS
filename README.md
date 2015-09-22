e2c71e00635ed0ed4574dcf6fbe1776cc8516cc8
austin <austin395.ad@gmail.com>
2015-09-22 18:35:14 -0400

    added readme and license as well as some additional comments in the code

---
+++ README.md
@@ -0,0 +1,21 @@
+INTRODUCTION
+------------
+LiKnox is a framework to be used with Debian for use as an IDS/IPS enabled router.
+This project is in it's infancy and is being used primarily as a tool for learning
+about network engineering, computer science, and programming.
+
+
+REQUIREMENTS
+------------
+This framework requires the following:
+  * Python 3.x
+  * Snort IDS
+  * Apache2
+  * IPtables
+  * more to follow
+
+
+INSTALLATION
+------------
+instructions will be added when relevant
+
--- snort listener.py
+++ snort listener.py
@@ -1,6 +1,7 @@
 __author__ = 'austin'
 
 #! /usr/bin/env python
+
 import os
 import socket
 import struct
@@ -11,7 +12,7 @@ ALERTMSG_LENGTH=256
 SNAPLEN=1500
 
 def main():
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)  # instantiates the socket object
 
     # This format does NOT include the 'Event' struct which is the last element
     # of the _AlertPkt struct in src/output-plugins/spo_alert_unixsock.h
@@ -21,40 +22,42 @@ def main():
     fmt_size = struct.calcsize(fmt)
 
     try:
-        os.remove("/dev/snort_alert")
+        os.remove("/dev/snort_alert")  # removes the old unix socket file if one exists
     except OSError:
         pass
 
-    s.bind("/dev/snort_alert")
+    s.bind("/dev/snort_alert")  # creates a unix socket
     while True:
         try:
             (datain, addr) = s.recvfrom(4096)
             start_time = time.time()  # testing
             (bmsg, ts_sec, ts_usec, caplen, pktlen, dlthdr, nethdr, transhdr, data, val, bpkt) = \
-            struct.unpack(fmt, datain[:fmt_size])
+            struct.unpack(fmt, datain[:fmt_size])  # unpacks the data from a received packet into the variables listed above
+
+            # ######## notes about each variable
             # bmsg      = alert message as byte object decode to UTF-8
-            # ts_sec    = !appears to be a time of some unknown format
-            # ts_usec   = !seems to always be 0?
-            # caplen    = !maybe the length of the stream that was captured. libpcap
-            # pktlen    = !seems to always be 0?
-            # dlthdr    = seems to always be 0? offset to data layer headers 2
-            # netdr     = seems to always be 14? offset to network layer header 3
-            # transdr   = seems to always be 34? offset to transport layer header 4
-            # data      = seems to always be 42? offset to data(pkt body)?
+            # ts_sec    = appears to be a time of some unknown format
+            # ts_usec   = seems to always be 0?
+            # caplen    = maybe the length of the stream that was captured. libpcap
+            # pktlen    = seems to always be 0?
+            # dlthdr    = offset to data layer headers 2
+            # netdr     = offset to network layer header 3
+            # transdr   = offset to transport layer header 4
+            # data      = offset to data(pkt body)?
             # val       = seems to always be 0?
             # pkt       = the packet raising the alert decode to UTF-8 may need further work as there are odd unicode characters
 
-            spkt = bpkt.decode('UTF-7', 'strict')
-            smsg = bmsg.decode('UTF-8', 'ignore')
-            print(spkt, '\n')
+            spkt = bpkt.decode('UTF-8', 'ignore')  # spkt for string packet and bpkt for byte packet. decode the byte format to something human readable
+            smsg = bmsg.decode('UTF-8', 'ignore')  # same as above
+            print(smsg, '\n', spkt)  # prints both of the above on separate lines
 
             # optionally, do something with the pcap pkthdr (ts_sec + ts_usec +
             # caplen + pktlen) and packet body (pkt)
             end_time = time.time()
-            print('running at a rate of ~{} alerts per second\n'.format(int(1/(end_time - start_time))))
+            print('running at a rate of ~{} alerts per second\n'.format(int(1.0/(end_time - start_time))))  # finds the rate of decoding and prints it to the screen
         except struct.error:
-            print("bad message? (msglen=%d): %s" % (len(datain)))
+            print("bad message? (msglen=%d): %s" % (len(datain)))  # handles the existences of a bad packet
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # executes the main program
     main()
