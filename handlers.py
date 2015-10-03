abb6178850f2d534fc462eb9b4b876f0bbcd658f
austin <austin395.ad@gmail.com>
2015-10-03 17:16:44 -0400

    added handlers.py file

---
+++ handlers.py
@@ -0,0 +1,31 @@
+__author__ = 'austin'
+
+import os
+import time
+import pickle
+import socket
+import sys
+import subprocess
+from netaddr import *
+import nmap
+import json
+
+class Handlers:
+    """
+    a class of static methods used by the main program based on the alerts recieved
+    """
+
+    @staticmethod
+    def nmap(network_address, scan_type):
+        nm = nmap.PortScanner()
+        options = {'fast': '-T5 -F', 'slow': '-p0-65535'}
+        if scan_type in options:
+            scan = options[scan_type]
+        else:
+            exit(9000)
+            print('an invalid scan type was passed to the function')
+        results = nm.scan(hosts=network_address, arguments=scan)
+        file = open('scan.json', 'w')
+        json.dump(results, file, indent=2, sort_keys=True)
+        file.close
+
